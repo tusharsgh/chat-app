2513a15eb2e03fe891389ebe9eb8c0b37dac617e
tushar singh <72065122+tusharsgh@users.noreply.github.com>
2023-09-25 23:51:01 +0530

    _

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 import logger from './configs/logger.config.js';
+import { log } from 'webpack/lib/node/nodeConsole.js';
 dotenv.config();
 //env values
 console.log(process.env.NODE_ENV)
 const PORT = process.env.PORT||8000;
-app.listen(PORT,()=>
+let server = app.listen(PORT,()=>
 {logger.info(`listening on port ${PORT}...`);
+  console.log("process id",process.pid)
+// throw new Error('error in server')
 });
+const exithandler =()=>{          
+    if(server){
+        logger.info("server closed");
+        process.exit(1);
+    }else{ 
+        process.exit(1);}
+}
+const caughtException=(error)=>{
+    logger.error(error);
+    exithandler();
+}
+process.on('uncaughtException',caughtException);
+process.on('unhandledException',caughtException);
